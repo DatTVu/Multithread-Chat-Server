965ebfd22daa6f03cfaaeff76e8e0304da5268de
DatTVu <dtvutrong1993@gmail.com>
2020-02-10 23:27:00 +0700

    server handle multiple connections

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -11,7 +11,8 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include "../utilities/utils.h"
-void handle_connection(int client_socket);
+#include <pthread.h>
+void *handle_connection(void* client_socket);
 int main(void)
 {
 	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
@@ -72,32 +73,40 @@ int main(void)
 	printf("server: waiting for connections...\n");
 
 
-	sin_size = sizeof(their_addr);
-	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-	if (new_fd == -1) {
-		perror("accept");
-		exit(1);
-	}
-
-	inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr),s, sizeof(s));
-	printf("server: got connection from %s\n", s);
 
 	while(1) {  // main accept() loop
-		handle_connection(new_fd);	
+		
+		sin_size = sizeof(their_addr);
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+		perror("accept");
+		exit(1);
+		}
+
+		inet_ntop(their_addr.ss_family,get_in_addr((struct sockaddr *)&their_addr),s, sizeof(s));
+		printf("server: got connection from %s\n", s);
+		//handle_connection(new_fd);
+		int *pclient = (int*)malloc(sizeof(int));
+		*pclient = new_fd;
+		pthread_t t;
+		pthread_create(&t, NULL, handle_connection, pclient);
 	}
 	return 0;
 }
 
-void handle_connection(int client_socket){
+void *handle_connection(void* p_client_socket){
+	int client_socket = *(int*)p_client_socket;
+	free(p_client_socket); //free the memory
 	char buf[MAXDATASIZE];
-	int result = recv(client_socket, buf, sizeof(buf), 0);
-	if (result == -1 || result == 0){
-		perror("recv");
-		exit(1);
-	}		
-	printf("number of bytes recv: %d", result);
-	printf("Message received: %s\n", buf);
-	if(strcmp(buf, "quit") == 0)
-		exit(1);	
+	while(1){
+		int result = recv(client_socket, buf, sizeof(buf), 0);
+		if (result == -1 || result == 0){
+			perror("recv");
+			break;
+		}		
+		buf[result] = '\0';
+		printf("\nMessage received: %s", buf);
+	}
+	return NULL;
 }
 
